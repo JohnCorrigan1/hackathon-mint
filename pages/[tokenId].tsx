31ee0568410a53e580ad3e8dd069719908947d4c
John Corrigan <jcorrigan322@gmail.com>
2023-03-04 15:07:03 -0700

    fix deployment

--- pages/[tokenId].tsx
+++ pages/[tokenId].tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps, NextPage } from "next";
 import Nav from "../components/Nav";
 import PlayerSheetCardOwned from "../components/Player/PlayerSheetCardOwned";
 import PlayerSheetItems from "../components/Player/PlayerSheetItems";
-import Stats from "../models/stats";
 import { useState, useEffect } from "react";
 import { getPlayerStats } from "../lib/SubgraphQueries";
 import { useRouter } from "next/router";
@@ -10,67 +9,52 @@ import { useQuery } from "urql";
 import { Player } from "../models/Player";
 
 interface Props {
-  tokenId: string | undefined | string[] ;
+  tokenId: string | undefined | string[];
 }
 
-
-const AssignStats = ({tokenId}: Props) => {
-
+const AssignStats = ({ tokenId }: Props) => {
   const [tokenID, setTokenID] = useState<string>(tokenId as string);
   const [query, setQuery] = useState<string>(getPlayerStats(tokenID) || "");
-  const [player, setPlayer] = useState<Player | null>(null)
+  const [player, setPlayer] = useState<Player | null>(null);
 
-  const mageStats: Stats = {
-    strength: 10,
-    defense: 10,
-    level: 1,
-    dexterity: 10,
-    luck: 10,
-    availablePoints: 0,
-    constitution: 10,
-    intelligence: 10,
-  };
+  const router = useRouter();
+  const playerId = router.query.tokenId;
 
-
-const router = useRouter();
-const playerId = router.query.tokenId;
-
-useEffect(() => {
+  useEffect(() => {
     setTokenID(playerId as string);
     setQuery(getPlayerStats(tokenID));
-}, [playerId, tokenID]);
+  }, [playerId, tokenID]);
 
-const [result, reexecuteQuery] = useQuery({
+  const [result, reexecuteQuery] = useQuery({
     query,
-    });
+  });
 
-    useEffect(() => {
-        const { data, fetching, error } = result;
-        console.log(result)
-        setPlayer(result.data?.players[0])
-        // console.log("player", player)
-    }, [result, player]);
+  useEffect(() => {
+    const { data, fetching, error } = result;
+    console.log(result);
+    setPlayer(result.data?.players[0]);
+  }, [result, player]);
 
-useEffect(() => {
+  useEffect(() => {
     reexecuteQuery();
-}, [tokenID]);
-    
+  }, [tokenID]);
+
   return (
     <div className="h-[100vh] w-[100vw] bg-[#2D2424]">
       <Nav />
       <div className="flex justify-center">
-      <div className="flex w-1/2 rounded-xl bg-[#D5CEA3] bg-opacity-30">
-        {player && (
-          <>
-        <PlayerSheetCardOwned
-          title={"title"}
-          items={[]}
-          player={player}
-          image={"/wizard.png"}
-        />
-        <PlayerSheetItems player={player} />
-        </>
-        )}
+        <div className="flex w-1/2 rounded-xl bg-[#D5CEA3] bg-opacity-30">
+          {player && (
+            <>
+              <PlayerSheetCardOwned
+                title={"title"}
+                items={[]}
+                player={player}
+                image={"/wizard.png"}
+              />
+              <PlayerSheetItems player={player} />
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -79,14 +63,12 @@ useEffect(() => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { tokenId } = context.query;
-  
+
   return {
     props: {
       tokenId,
     },
   };
 };
-
-
 
 export default AssignStats;
