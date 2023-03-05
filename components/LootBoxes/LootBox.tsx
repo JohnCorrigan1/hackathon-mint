731f0ee20ed88e39e7cc7987bb2b4dc730925259
John Corrigan <jcorrigan322@gmail.com>
2023-03-04 17:12:55 -0700

    dumb animation

--- components/LootBoxes/LootBox.tsx
+++ components/LootBoxes/LootBox.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
+import { Dispatch, SetStateAction } from "react"
 
-
-const LootBox = () => {
+const LootBox: React.FC<{isAnimating: boolean, setIsAnimating: Dispatch<SetStateAction<boolean>>}> = (props) => {
     return (
         <div className="flex justify-center items-center">
-            <Image src="/crate.png" height={300} width={300}  /> 
-        </div>             
+            <Image src="/crateCropped.png" height={300} width={300} className={"" + (props.isAnimating ? " crate" : "")} /> 
+        </div>
     )
 }
 
--- pages/mintgear.tsx
+++ pages/mintgear.tsx
@@ -1,17 +1,27 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
 import Description from '../components/LootBoxes/Description'
 import LootBox from '../components/LootBoxes/LootBox'
 import Nav from '../components/Nav'
 
 const mintgear: NextPage = () => {
+
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    const handleGearMint = () => {
+        setIsAnimating(true)
+    }
+
     return (
         <div className='h-[100vh] bg-[#2D2424] '>
             <Nav />
-            <div className='w-full flex justify-center  items-center'>
-            <div className='bg-[#D5CEA3] flex justify-center items-center w-1/3 bg-opacity-40 mt-auto'>
-                <LootBox />
+            <div className='w-full flex flex-col justify-center  items-center'>
+            <div className='bg-[#D5CEA3] flex justify-center items-center w-1/3 bg-opacity-40 mt-auto p-10 gap-10'>
+                <LootBox isAnimating={isAnimating} setIsAnimating={setIsAnimating} />
                 <Description />
+                
             </div>
+            <button onClick={handleGearMint} className='bg-blue-500 hover:bg-blue-600 active:scale-95 hover:scale-105 text-zinc-200 p-2 mt-5 w-1/5 rounded-lg font-bold text-xl'>Mint</button>
             </div>
         </div>
     )
